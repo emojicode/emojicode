0a13e6f37bba70cf99bca86a9457e028cd6effbf
thbwd <hello@tbw.design>
2018-11-13 19:16:18 +0100

    🍇 Test .🍇 extension

--- tests.py
+++ tests.py
@@ -8,107 +8,108 @@ import re
 quick = len(sys.argv) > 2 and sys.argv[2] == 'quick'
 
 compilation_tests = [
-    "hello",
-    "print",
-    "intTest",
-    "if",
-    "vars",
-    "repeatedPackageImport",
-    "enum",
-    "enumMethod",
-    "enumTypeMethod",
-    "unwrap",
-    "imperativeInterrogative",
-    "assignmentByCall",
-    "repeatWhile",
-    "conditionalProduce",
-    "stringConcat",
-    "babyBottleInitializer",
-    "classInheritance",
-    "classOverride",
-    "classSuper",
-    "classSubInstanceVar",
-    "optionalParameter",
-    "returnInBlock",
-    "returnInIf",
-    "identityOperator",
-    "typesAsValues",
-    "class",
-    "privateClassMethod",
+    "hello.emojic",
+    "hello.🍇",
+    "print.emojic",
+    "intTest.emojic",
+    "if.emojic",
+    "vars.emojic",
+    "repeatedPackageImport.emojic",
+    "enum.emojic",
+    "enumMethod.emojic",
+    "enumTypeMethod.emojic",
+    "unwrap.emojic",
+    "imperativeInterrogative.emojic",
+    "assignmentByCall.emojic",
+    "repeatWhile.emojic",
+    "conditionalProduce.emojic",
+    "stringConcat.emojic",
+    "babyBottleInitializer.emojic",
+    "classInheritance.emojic",
+    "classOverride.emojic",
+    "classSuper.emojic",
+    "classSubInstanceVar.emojic",
+    "optionalParameter.emojic",
+    "returnInBlock.emojic",
+    "returnInIf.emojic",
+    "identityOperator.emojic",
+    "typesAsValues.emojic",
+    "class.emojic",
+    "privateClassMethod.emojic",
     # "extension",
-    "assignmentByCallInstanceVariable",
-    "valueType",
-    "valueTypeSelf",
-    "valueTypeMutate",
-    "compareNoValue",
-    "downcastClass",
-    "castAny",
+    "assignmentByCallInstanceVariable.emojic",
+    "valueType.emojic",
+    "valueTypeSelf.emojic",
+    "valueTypeMutate.emojic",
+    "compareNoValue.emojic",
+    "downcastClass.emojic",
+    "castAny.emojic",
     # "castGenericValueType",
-    "protocolClass",
-    "protocolSubclass",
-    "protocolValueType",
-    "protocolValueTypeRemote",
-    "protocolEnum",
-    "protocolGenericLayerClass",
-    "protocolGenericLayerValueType",
-    "protocolMulti",
-    "reboxToSomething",
-    "assignmentByCallProtocol",
-    "commonType",
-    "generics",
-    "genericsValueType",
-    "genericProtocol",
-    "genericProtocolValueType",
-    "genericTypeMethod",
-    "genericLocalAsArgToGeneric",
-    "variableInitAndScoping",
-    "valueTypeRemoteAdditional",
-    "closureBasic",
-    "closureCapture",
-    "closureCaptureThis",
-    "closureCaptureValueType",
-    "closureCaptureThisClass",
-    "callableBoxing",
-    "errorIsError",
-    "errorUnwrap",
-    "errorAvocado",
-    "errorHandlerMemory",
-    "errorInitializer",
-    "valueTypeCopySelf",
-    "valueTypeBoxCopySelf",
-    "includer",
-    "threads",
-    "inferListLiteralFromExpec",
-    "sequenceTypeNames",
-    "typeValues",
-    "rcOrder",
-    "rcOrderVt",
-    "rcTempOrder",
-    "rcInstanceVariable",
-    "rcOnlyReference",
-    "identifierTest",
+    "protocolClass.emojic",
+    "protocolSubclass.emojic",
+    "protocolValueType.emojic",
+    "protocolValueTypeRemote.emojic",
+    "protocolEnum.emojic",
+    "protocolGenericLayerClass.emojic",
+    "protocolGenericLayerValueType.emojic",
+    "protocolMulti.emojic",
+    "reboxToSomething.emojic",
+    "assignmentByCallProtocol.emojic",
+    "commonType.emojic",
+    "generics.emojic",
+    "genericsValueType.emojic",
+    "genericProtocol.emojic",
+    "genericProtocolValueType.emojic",
+    "genericTypeMethod.emojic",
+    "genericLocalAsArgToGeneric.emojic",
+    "variableInitAndScoping.emojic",
+    "valueTypeRemoteAdditional.emojic",
+    "closureBasic.emojic",
+    "closureCapture.emojic",
+    "closureCaptureThis.emojic",
+    "closureCaptureValueType.emojic",
+    "closureCaptureThisClass.emojic",
+    "callableBoxing.emojic",
+    "errorIsError.emojic",
+    "errorUnwrap.emojic",
+    "errorAvocado.emojic",
+    "errorHandlerMemory.emojic",
+    "errorInitializer.emojic",
+    "valueTypeCopySelf.emojic",
+    "valueTypeBoxCopySelf.emojic",
+    "includer.emojic",
+    "threads.emojic",
+    "inferListLiteralFromExpec.emojic",
+    "sequenceTypeNames.emojic",
+    "typeValues.emojic",
+    "rcOrder.emojic",
+    "rcOrderVt.emojic",
+    "rcTempOrder.emojic",
+    "rcInstanceVariable.emojic",
+    "rcOnlyReference.emojic",
+    "identifierTest.emojic",
 ]
 
 if not quick:
     compilation_tests.extend([
-      "stressTest1",
-      "stressTest2",
-      "stressTest3",
-      "stressTest4"
+      "stressTest1.emojic",
+      "stressTest2.emojic",
+      "stressTest3.emojic",
+      "stressTest4.emojic"
     ])
 
 library_tests = [
-    "primitives",
-    "mathTest",
-    "rangeTest",
-    "stringTest",
-    "dataTest",
-    "systemTest",
-    "listTest",
-    "enumerator",
-    "dictionaryTest",
+    "primitives.emojic",
+    "mathTest.emojic",
+    "rangeTest.emojic",
+    "stringTest.emojic",
+    "dataTest.emojic",
+    "systemTest.emojic",
+    "listTest.emojic",
+    "enumerator.emojic",
+    "dictionaryTest.emojic",
     # "jsonTest",
-    "fileTest"
+    "fileTest.emojic"
 ]
 reject_tests = glob.glob(os.path.join(dist.source, "tests", "reject",
                                       "*.emojic"))
@@ -126,8 +127,9 @@ def fail_test(name):
 
 
 def test_paths(name, kind):
-    return (os.path.join(dist.source, "tests", kind, name + ".emojic"),
-            os.path.join(dist.source, "tests", kind, name))
+    root, ext = os.path.splitext(name)
+    return (os.path.join(dist.source, "tests", kind, name),
+            os.path.join(dist.source, "tests", kind, root))
 
 
 def library_test(name):
@@ -144,7 +146,8 @@ def compilation_test(name):
     source_path, binary_path = test_paths(name, 'compilation')
     run([emojicodec, source_path, '-O'], check=True)
     completed = run([binary_path], stdout=PIPE)
-    exp_path = os.path.join(dist.source, "tests", "compilation", name + ".txt")
+    exp_path = os.path.join(dist.source, "tests", "compilation",
+                            os.path.splitext(name)[0] + ".txt")
     output = completed.stdout.decode('utf-8')
     if output != open(exp_path, "r", encoding='utf-8').read():
         print(output)
@@ -162,10 +165,12 @@ def reject_test(filename):
 def available_compilation_tests():
     paths = glob.glob(os.path.join(dist.source, "tests", "compilation",
                                    "*.emojic"))
+    paths.extend(glob.glob(os.path.join(dist.source, "tests", "compilation",
+                                   "*.🍇")))
 
-    map_it = map(lambda f: os.path.splitext(os.path.basename(f))[0], paths)
+    map_it = map(lambda f: os.path.basename(f), paths)
     tests = list(map_it)
-    tests.remove('included')
+    tests.remove('included.emojic')
     return tests
 
 
